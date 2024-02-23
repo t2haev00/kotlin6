6e77c5c57944043faa4c688cabe69ced7ff500e4
t2haev00 <eveliinahampus@gmail.com>
2024-02-24 01:24:53 +0200

    feat: Scaffold nav

--- app/src/main/java/com/eveliina/scaffoldapp/MainActivity.kt
+++ app/src/main/java/com/eveliina/scaffoldapp/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.eveliina.scaffoldapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,6 +17,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.eveliina.scaffoldapp.ui.theme.ScaffoldAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,30 +34,45 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+//@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScaffoldApp() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "Home"
+    ) {
+        composable(route = "Home") {
+            MainScreen(navController)
+        }
+        composable(route = "Info") {
+            InfoScreen(navController)
+        }
+        composable(route = "Settings") {
+            SettingsScreen(navController)
+        }
+    }
+}
+
+/*
     Scaffold(
         topBar = { TopAppBar { Text(text="My app") }},
         content = { Text(text="Content for home screen") },
         //bottomBar = { BottomAppBar { Text(text="Bottom bar")}}
-    )
-}
+)
+*/
 
+// TOP & SCREEN BARS
+//
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTopBar() {
+fun MainTopBar(title: String,navController: NavController) {
     var expanded by remember { mutableStateOf(false)}
 
     TopAppBar(
-        title = { Text("MyApp")},
-        navigationIcon = {
-            IconButton(
-                onClick = { /*TODO*/ },
-            ){
-                Icon(Icons.Filled.Menu, contentDescription =null)
-            }
-        },
+        title = { Text(title)},
         actions = {
             IconButton(
                 onClick = {
@@ -65,16 +84,58 @@ fun MyTopBar() {
             DropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded = false }) {
-                DropwdownMenuItem(onClick = { /*TODO*/ }) {
+                DropwdownMenuItem(onClick = { navController.navigate("info") }) {
                     Text("Info")
                 }
-                DropwdownMenuItem(onClick = { /*TODO*/ }) {
+                DropwdownMenuItem(onClick = { navController.settings("settings") }) {
                     Text("Settings")
                 }
             }
         }
     )
 }
+
+@Composable
+fun ScreenTopBar(title: String, navController: NavController){
+    TopAppBar(
+        title= { Text(title)},
+        navigationIcon = {
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(Icons.Filled.Arrowback, contentDescription = null)
+            }
+        }
+    )
+}
+
+// MAIN, INFO and SETTINGS SCREENS
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen(navController: NavController) {
+    Scaffold(
+        topBar = { MainTopBar("My app", navController) },
+        content = { Text(text="Content for Home screen")},
+    )
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun InfoScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ScreenTopBar("Info", navController) },
+        content = { Text(text="Content for Info screen")},
+    )
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun SettingsScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ScreenTopBar("Settings", navController) },
+        content = { Text(text="Content for Settings screen")},
+    )
+}
+
 
 
 @Preview(showBackground = true)
